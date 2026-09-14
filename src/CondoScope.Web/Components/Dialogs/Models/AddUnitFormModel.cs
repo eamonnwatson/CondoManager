66c4d572d5f3347f74624369d2602b23b9a3b1d0
Eamonn Watson <github@epw.email>
2026-09-13 22:02:39 -0300

    Expand web pages for units, owners, and fee charges

---
+++ src/CondoScope.Web/Components/Dialogs/Models/AddUnitFormModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CondoScope.Web.Components.Dialogs.Models;
+
+internal class AddUnitFormModel
+{
+    [Required]
+    public string UnitNumber { get; set; } = default!;
+    [Required]
+    public string Address { get; set; } = default!;
+}
