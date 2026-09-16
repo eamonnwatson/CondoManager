7a4245cb22c374ccc9cf46e8284e754b5df130b6
Eamonn Watson <github@epw.email>
2026-09-15 22:43:47 -0300

    Namespace change

--- src/CondoScope.Application/Units/UnitDto.cs
+++ src/CondoScope.Application/Units/UnitDto.cs
@@ -1,3 +1,3 @@
-﻿namespace CondoScope.Application.Units.Queries;
+﻿namespace CondoScope.Application.Units;
 
 public record UnitDto(string UnitId, string UnitNumber, string Address, string CurrentOwnerId, string CurrentOwnerName);
