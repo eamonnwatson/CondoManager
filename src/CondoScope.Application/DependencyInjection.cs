1e564557d892ecfafe90d21ca501a8375616e2c1
Eamonn Watson <github@epw.email>
2026-09-15 22:42:37 -0300

    Remove legacy ResultExtensions and add dependency injection configuration

---
+++ src/CondoScope.Application/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using CondoScope.Application.Common.Mapping;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CondoScope.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var currentAssembly = typeof(DependencyInjection).Assembly;
+        services.AddSingleton<IMapper>(_ => Mapper.Build(m => RegisterAllMapModules(m, currentAssembly)));
+
+        return services;
+    }
+
+
+    private static void RegisterAllMapModules(Mapper mapper, Assembly? assembly)
+    {
+        ArgumentNullException.ThrowIfNull(assembly);
+
+        var moduleTypes = assembly
+            .GetTypes()
+            .Where(t => !t.IsAbstract && !t.IsInterface && typeof(IMapModule).IsAssignableFrom(t));
+
+        foreach (var type in moduleTypes)
+        {
+            var module = (IMapModule)Activator.CreateInstance(type)!;
+            module.RegisterMaps(mapper);
+        }
+    }
+
+}
